3693afcf036e3324d552ac7ac010ea990545b91d
Gurov Oleg <oleg.ispu@yandex.ru>
2016-06-01 14:29:10 +0300

    добавлена pg_store_proc_descr

--- sql/ws/12_type.sql
+++ sql/ws/12_type.sql
@@ -15,3 +15,6 @@ SELECT pg_c('d','d_code', 'Имя переменной');
 
 CREATE DOMAIN d_errcode AS char(5) CHECK (VALUE ~ E'^Y\\d{4}$') ;
 SELECT pg_c('d','d_errcode', 'Код ошибки');
+
+CREATE TYPE t_textarr as (fld TEXT[]);
+SELECT pg_c('t','t_textarr', 'Вспомогательный тип данных для ф-ции pg_store_proc_descr');
