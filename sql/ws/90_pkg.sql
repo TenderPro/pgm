9f8a06274f38460c7b32043c958aadeefa358e70
Gurov Oleg <oleg.ispu@yandex.ru>
2016-06-01 15:28:40 +0300

    добавлен тест pg_store_proc_descr

---
+++ sql/ws/90_pkg.sql
@@ -0,0 +1,11 @@
+/*
+
+    Copyright (c) 2010, 2012 Tender.Pro http://tender.pro.
+    [SQL_LICENSE]
+
+    Тестирование пакета ws
+*/
+
+/* ------------------------------------------------------------------------- */
+SELECT * FROM ws.pg_store_proc_descr('ws') WHERE name = 'pg_store_proc_descr' ORDER BY name ASC;
+/* ------------------------------------------------------------------------- */
