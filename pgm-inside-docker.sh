d8258a650256d3157ce207830fb50574ab9cd144
Alexey Kovrizhkin <jean@tender.pro>
2016-08-21 09:01:55 +0300

    добавлена поддержка pgm/sql

--- pgm-inside-docker.sh
+++ pgm-inside-docker.sh
@@ -3,6 +3,9 @@
 # Пример вызова:
 #    source .config && \
 #      docker exec -ti "pgrest_${APP_SITE}_www" bash /home/app/pgm/pgm-inside-docker.sh /home/app $@
+#
+#  работа с пакетами из DIR/sql (каталог опознается по суффиксу /sql)
+#      docker exec -ti "pgrest_${APP_SITE}_www" bash /home/app/pgm/pgm-inside-docker.sh /home/app DIR/sql $@
 
 user=$APPUSER
 [[ "$user" ]] || user=$USER
@@ -13,4 +16,12 @@ cd $ROOT
 
 PGM=pgm
 [ -f pgm/pgm.sh ] && PGM=pgm/pgm.sh
-gosu $user bash $PGM $@
+
+dir=""
+arg=$1
+if [[ "$arg" != "${arg%/sql}" ]]; then
+  dir=$arg
+  shift
+fi
+
+SQLROOT=$dir gosu $user bash $PGM $@
