f0420232722d16c25b968405fba060f03aec22c1
Alexey Kovrizhkin <jean@tender.pro>
2016-10-24 16:23:32 +0300

    ф-и перенесены в файлы, доступные по make

--- sql/ws/12_type.sql
+++ sql/ws/12_type.sql
@@ -8,13 +8,9 @@
 
 /* ------------------------------------------------------------------------- */
 CREATE DOMAIN d_id AS INTEGER;
-SELECT pg_c('d','d_id', 'Идентификатор');
 
 CREATE DOMAIN d_code AS TEXT CHECK (VALUE ~ E'^[a-z\\d][a-z\\d\\.\\-_]*$') ;
-SELECT pg_c('d','d_code', 'Имя переменной');
 
 CREATE DOMAIN d_errcode AS char(5) CHECK (VALUE ~ E'^Y\\d{4}$') ;
-SELECT pg_c('d','d_errcode', 'Код ошибки');
 
 CREATE TYPE t_textarr as (fld TEXT[]);
-SELECT pg_c('t','t_textarr', 'Вспомогательный тип данных для ф-ции pg_store_proc_descr');
