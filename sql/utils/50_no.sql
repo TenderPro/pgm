9ff3a7ce023addfa24e60382ef7496bebb45db7b
Alexey Kovrizhkin <jean@tender.pro>
2017-08-14 16:56:16 +0300

    проверки отсутствия объектов БД

---
+++ sql/utils/50_no.sql
@@ -0,0 +1,46 @@
+/*
+  Служебные функции
+  Проверки отсутствия объектов БД
+*/
+
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION no_schema(name) RETURNS BOOL LANGUAGE 'sql' AS
+$_$
+  SELECT NOT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = $1)
+$_$;
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION no_domain(name) RETURNS BOOL LANGUAGE 'sql' AS
+$_$
+  SELECT NOT EXISTS(SELECT 1 FROM information_schema.domains WHERE domain_schema = any(current_schemas(false)) AND domain_name = $1)
+$_$;
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION no_type(name) RETURNS BOOL LANGUAGE 'sql' AS
+$_$
+  SELECT NOT EXISTS(SELECT 1 FROM information_schema.user_defined_types WHERE user_defined_type_schema = any(current_schemas(false)) AND user_defined_type_name = $1)
+$_$;
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION no_table(name) RETURNS BOOL LANGUAGE 'sql' AS
+$_$
+  SELECT NOT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = any(current_schemas(false)) AND table_name = $1)
+$_$;
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION no_sequence(name) RETURNS BOOL LANGUAGE 'sql' AS
+$_$
+  SELECT NOT EXISTS(SELECT 1 FROM information_schema.sequences WHERE sequence_schema = any(current_schemas(false)) AND sequence_name = $1)
+$_$;
+
+/* ------------------------------------------------------------------------- */
+CREATE OR REPLACE FUNCTION empty_table(name) RETURNS BOOL LANGUAGE 'plpgsql' AS
+$_$
+  DECLARE
+    v_ret BOOL;
+  BEGIN
+    EXECUTE 'SELECT (SELECT 1 FROM ' || $1 || ' LIMIT 1) IS NULL' INTO v_ret;
+    RETURN v_ret;
+  END;
+$_$;
